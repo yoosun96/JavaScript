b312fe524ccb574c1ca7c74069aa2bc68bf0e6c2
yoosun96 <yoosnu@naver.com>
2021-06-18 23:36:54 +0900

    add commit

--- dom/shopping/main.js
+++ dom/shopping/main.js
@@ -13,7 +13,9 @@ function onAdd() {
   const item = createItem(text);
   // 3. items 컨테이너안에 새로 만든 아이템을 추가한다
   items.appendChild(item);
-  // 4. input을 초기화 한다.
+  // 4. 새로 추가된 아이템으로 스크롤링
+  item.scrollIntoView({ block: 'center' });
+  // 5. input을 초기화 한다.
   input.value = '';
   input.focus();
 }
@@ -52,5 +54,7 @@ addBtn.addEventListener('click', () => {
 });
 
 input.addEventListener('keypress', (event) => {
-  console.log('key');
+  if (event.key === 'Enter') {
+    onAdd();
+  }
 });
