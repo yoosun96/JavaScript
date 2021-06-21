89a3c19c2d5d51a4c0add007b0a07ae94d4f99ea
yoosun96 <yoosnu@naver.com>
2021-06-21 22:02:25 +0900

    add commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// number, string, boolean, null. undefined
+// let number = 2;
+// let number2 = number;
+// //console.log(number);
+// //console.log(number2);
+
+// number2 = 3;
+// //console.log(number);
+// //console.log(number2);
+
+
+// object
+// let obj = {
+//   name: 'yoosun',
+//   age: '26',
+// };
+// console.log(obj.name);
+
+// let obj2 = obj;
+
+// console.log(obj2.name);
+
+// obj.name = 'james';
+// console.log('--------');
+// console.log(obj.name);
+// console.log(obj2.name);
